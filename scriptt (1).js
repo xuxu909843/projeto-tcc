353a927fa60482a24e9b53231182828d9326edac
JU <160606742+xuxu909843@users.noreply.github.com>
2024-11-25 08:59:11 -0300

    Add files via upload

---
+++ scriptt (1).js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+        
+    });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function () {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop+ section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+    });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.dish', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#testimonial_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+
+
+    // Script para o menu mobile
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenu = document.getElementById("mobile_menu");
+    const navList = document.getElementById("nav_list");
+
+    mobileMenu.addEventListener("click", function () {
+        navList.classList.toggle("active");
+    });
+});
+
+
+// ScrollReveal para animações
+ScrollReveal().reveal('#cta', { delay: 200, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.dish', { delay: 300, distance: '50px', origin: 'left', duration: 1000, interval: 200 });
+
+
+});
